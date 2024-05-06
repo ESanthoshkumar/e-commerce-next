78a03325c2adb429fa6c26dea7c899f2d10c36a3
ESanthoshkumar <152504455+ESanthoshkumar@users.noreply.github.com>
2024-05-06 18:53:16 +0530

    shopping cart

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,10 +1,11 @@
-"use client"
+// Importing necessary modules
 import React, { useContext } from 'react';
 import { AppContext } from '../utility/context/context-api';
 import Link from 'next/link';
 
-export default function page() {
-    const { cart } = useContext(AppContext)
+// Corrected component name and usage of useContext hook
+const CartPage = () => {
+    const { cart } = useContext(AppContext);
 
     return (
         <div className='bg-gradient-to-b from-black to-gray-200 h-[screen] py-8 '>
@@ -17,18 +18,21 @@ export default function page() {
                 <Link className=' bg-white font-semibold hover:bg-rose-400 hover:text-gray-700 border-black border-2  px-8 py-1 rounded-md hover:border-rose-200 duration-300' href='/'>Back To Home</Link>
             </div>
         </div>
-    )
+    );
 }
 
-const ProductCard = ({ pro }) => {
+// Exporting the component with the corrected name
+export default CartPage;
 
+// Renamed the component function to start with an uppercase letter
+const ProductCard = ({ pro }) => {
     return (
         <div className='flex justify-center items-center '>
             <div>
                 <img className=' h-[300px] w-[450px] rounded-md ' src={pro.image} alt={`${pro.id}`} />
                 <div className='flex justify-between items-center text-blue-800'>
                     <p className='text-xl text-right font-semibold'>{pro.name}</p>
-                    <p className='  '>{pro.price}</p>
+                    <p className=''>{pro.price}</p>
                 </div>
 
                 <div className='flex justify-center items-center py-4'>
@@ -40,5 +44,5 @@ const ProductCard = ({ pro }) => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
